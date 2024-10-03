dd9763528ab34ff86a5248221dbbf2f2e6e68dbf
Flavio Calvo <Flavio.Calvo@unil.ch>
2024-10-03 17:13:33 +0200

    The `permutation` encoder/decoder now accepts either a permutation (in the form of an alternative alphabet), or a `CryptoNumber` (then mapped to an alternative alphabet) as a key; the lookup table is now also properly updated by the decoder (bugfix)

--- src/scytale/cyphers/permutation.py
+++ src/scytale/cyphers/permutation.py
@@ -8,17 +8,31 @@
 A permutation cipher
 
 A map key->permutation allows to build a permutation cipher out
-of a provided key
+of a provided key, which can either be a new (reordered) alphabet,
+or a `CryptoNumber`, in which case the key is mapped to an arbitrary
+permutation of the alphabet.
 """
 
 
 def encoder(n, pos, key):
     from ..tools.alphabet import char2num, get_alphabet_permutation
+    from ..tools.alphaencoder import CryptoNumber
 
+    if isinstance(key, CryptoNumber):
+        is_cryptonumber = True
+    else:
+        is_cryptonumber = False
     try:
-        permutation = encoder.permutations[key]
+        if is_cryptonumber:
+            permutation = encoder.permutations[tuple(key)]
+        else:
+            permutation = encoder.permutations[key]
     except KeyError:
-        new_alphabet = get_alphabet_permutation(key)
+        if is_cryptonumber:
+            new_alphabet = get_alphabet_permutation(str(key))
+            key = tuple(key)
+        else:
+            new_alphabet = key
         permutation = [char2num(c) for c in new_alphabet]
         encoder.permutations[key] = permutation
     return permutation[n]
@@ -29,15 +43,28 @@ encoder.permutations = {}
 
 def decoder(n, pos, key):
     from ..tools.alphabet import char2num, get_alphabet_permutation
+    from ..tools.alphaencoder import CryptoNumber
 
+    if isinstance(key, CryptoNumber):
+        is_cryptonumber = True
+    else:
+        is_cryptonumber = False
     try:
-        permutation = decoder.permutations[key]
+        if is_cryptonumber:
+            permutation = decoder.permutations[tuple(key)]
+        else:
+            permutation = decoder.permutations[key]
     except KeyError:
-        new_alphabet = get_alphabet_permutation(key)
+        if is_cryptonumber:
+            new_alphabet = get_alphabet_permutation(str(key))
+            key = tuple(key)
+        else:
+            new_alphabet = key
         inv_permutation = [char2num(c) for c in new_alphabet]
         permutation = len(new_alphabet) * [0]
         for i in inv_permutation:
             permutation[inv_permutation[i]] = i
+        decoder.permutations[key] = permutation
     return permutation[n]
 
 
